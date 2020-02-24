d0ed6a6028ca0641c2cf63610d69f618f0b6dbbe
Mostafa <mostafatarek15894@gmail.com>
2020-02-24 23:45:26 +0200

    send order

---
+++ AwfarlakOriginal/Senarios/NavigationViews/Bar/My Cart/Model/SendOrder.swift
@@ -0,0 +1,22 @@
+//
+//  SendOrder.swift
+//  AwfarlakOriginal
+//
+//  Created by Mustafa on 2/24/20.
+//  Copyright © 2020 amirahmed. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - SendOrder
+struct SendOrder: Codable {
+    let status: Int
+    let idOrder: String?
+    let  message: String
+
+    enum CodingKeys: String, CodingKey {
+        case status
+        case idOrder = "id_order"
+        case message
+    }
+}
